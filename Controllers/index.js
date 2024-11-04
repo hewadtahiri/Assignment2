7faffde3fb72e59662d43e4d16a3db603529b20b
Hewad Tahiri <hewad.tahiri@ontariotechu.net>
2024-11-04 02:30:40 -0500

    Stylesheet testing.

--- Controllers/index.js
+++ Controllers/index.js
@@ -2,19 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("Layout", { title: "Home", body: "<%- include('Home') %>" });
+  res.render("Layout", { title: "Home", body: "Home" });
 });
 
 router.get("/AboutMe", (req, res) => {
-  res.render("Layout", { title: "About Me", body: "<%- include('About Me') %>" });
+  res.render("Layout", { title: "About Me", body: "About Me" });
 });
 
 router.get("/ContactMe", (req, res) => {
-  res.render("Layout", { title: "Contact Me", body: "<%- include('Contact Me') %>" });
+  res.render("Layout", { title: "Contact Me", body: "Contact Me" });
 });
 
 router.get("/Projects", (req, res) => {
-  res.render("Layout", { title: "Projects", body: "<%- include('Projects') %>" });
+  res.render("Layout", { title: "Projects", body: "Projects" });
 });
 
 module.exports = router;
