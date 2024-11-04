61753a3830367b537138446e41a16414db2d7094
Hewad Tahiri <hewad.tahiri@ontariotechu.net>
2024-11-04 02:28:27 -0500

    Updated index.js.

--- Controllers/index.js
+++ Controllers/index.js
@@ -2,19 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("Home", { title: "Home", body: "<%- include('Home') %>" });
+  res.render("Layout", { title: "Home", body: "<%- include('Home') %>" });
 });
 
 router.get("/AboutMe", (req, res) => {
-  res.render("About Me", { title: "About Me", body: "<%- include('About Me') %>" });
+  res.render("Layout", { title: "About Me", body: "<%- include('About Me') %>" });
 });
 
 router.get("/ContactMe", (req, res) => {
-  res.render("Contact Me", { title: "Contact Me", body: "<%- include('Contact Me') %>" });
+  res.render("Layout", { title: "Contact Me", body: "<%- include('Contact Me') %>" });
 });
 
 router.get("/Projects", (req, res) => {
-  res.render("Projects", { title: "Projects", body: "<%- include('Projects') %>" });
+  res.render("Layout", { title: "Projects", body: "<%- include('Projects') %>" });
 });
 
 module.exports = router;
