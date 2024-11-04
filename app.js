52d03876fc8c1438dfb92fb98f28d8645c793d20
Hewad Tahiri <hewad.tahiri@ontariotechu.net>
2024-11-04 01:50:58 -0500

    Updated app.js and server.js files.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
-const routes = require('./Controllers/index');
+const express = require("express");
+const path = require("path");
+const routes = require("./Controllers/index");
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'Views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "Views"));
 
-app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.static(path.join(__dirname, "Public")));
 
-app.use('/', routes);
+app.use("/", routes);
 
 module.exports = app;
